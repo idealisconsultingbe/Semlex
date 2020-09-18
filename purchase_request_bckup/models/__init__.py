4eb5161cc5ff89f94430bf6cb4f05a7ecc87fd75
Benjamin Dugardin <bdu@idealisconsulting.com>
2020-09-18 14:50:44 +0200

    [DEL] remove backup

--- purchase_request_bckup/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import purchase_request
-from . import purchase_request_tag
-from . import purchase_request_stage
-from . import purchase_request_line
-from . import purchase_order
